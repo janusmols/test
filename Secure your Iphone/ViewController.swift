876c55b1368411b63951fcd3002bed3154442c75
janusmols <janusbm@gmail.com>
2015-05-16 17:07:10 +0200

    Initial Commit

---
+++ Secure your Iphone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Secure your Iphone
+//
+//  Created by Janus Broch Mols on 16/05/15.
+//  Copyright (c) 2015 Janus Broch Mols. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
