205da7f1bf607dc66afc70c261ba657bf2b02413
janusmols <janusbm@gmail.com>
2015-05-16 18:53:41 +0200

    .

---
+++ Secure your Iphone/Accelerometer.swift
@@ -0,0 +1,59 @@
+//
+//  Accelerometer.swift
+//  Secure your Iphone
+//
+//  Created by Janus Broch Mols on 16/05/15.
+//  Copyright (c) 2015 Janus Broch Mols. All rights reserved.
+//
+
+import Foundation
+import CoreMotion
+
+class Accelerometer {
+    
+    lazy var motionManager = CMMotionManager()
+    
+    var x:Double = 0.000
+    var y:Double = 0.000
+    var z:Double = 0.000
+    
+    
+    func retrieveAccelerometerData(){
+        
+    let queue = NSOperationQueue()
+    motionManager.startAccelerometerUpdatesToQueue(queue, withHandler:
+    {(data: CMAccelerometerData!, error: NSError!) in
+    
+    println("X = \(data.acceleration.x)")
+    println("Y = \(data.acceleration.y)")
+    println("Z = \(data.acceleration.z)")
+    
+    self.x = data.acceleration.x
+    self.y = data.acceleration.y
+    self.z = data.acceleration.z
+    
+        
+        var timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("checkAccelerometer"), userInfo: nil, repeats: true)
+        timer.invalidate()
+    }
+    )
+    
+    
+}
+
+
+
+
+func checkAccelerometer(){
+    if x > 0.09 || y > 0.09 || z > 0.09{
+        
+    }
+    
+    
+}
+
+
+
+}
+
+
---
+++ Secure your Iphone/HomeScreen.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  Secure your Iphone
+//
+//  Created by Janus Broch Mols on 16/05/15.
+//  Copyright (c) 2015 Janus Broch Mols. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+class HomeScreen: UIViewController {
+    var accelerometerAvalible: Bool?
+    lazy var motionManager = CMMotionManager()
+
+    @IBOutlet weak var goActiveBtn: UIButton!
+    @IBOutlet weak var goOflineBtn: UIButton!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if motionManager.accelerometerAvailable{
+            accelerometerAvalible = true
+                    }
+        else{
+            accelerometerAvalible = false
+            println("Accelerometer is not available, Your device need to have a accelerometer.")
+        }
+        
+}
+    
+
+    
+    @IBAction func GoActive(sender: AnyObject) {
+        
+        if accelerometerAvalible == true{
+        
+            //Accelerometer().retrieveAccelerometerData()
+            
+            var timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: Accelerometer(), selector: Selector("retrieveAccelerometerData"), userInfo: nil, repeats: false)
+            
+        }
+}
+    
+    @IBAction func GoOfline(sender: AnyObject) {
+        
+    }
+    
+    
+    
+}
+
+
+   
+    
+    
+
+
+
+
+
+
+
+
+
+
