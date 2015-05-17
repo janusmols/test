ec32f5609ff2f66006bbd5f3ab174b3b0a8c8ce3
janusmols <janusbm@gmail.com>
2015-05-17 11:23:51 +0200

    .

---
+++ Secure your Iphone/Bridging header.m
@@ -0,0 +1,9 @@
+//
+//  Bridging header.m
+//  Secure your Iphone
+//
+//  Created by Janus Broch Mols on 17/05/15.
+//  Copyright (c) 2015 Janus Broch Mols. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
--- Secure your Iphone/Secure.m
+++
@@ -1,9 +0,0 @@
-//
-//  Secure.m
-//  Secure your Iphone
-//
-//  Created by Janus Broch Mols on 17/05/15.
-//  Copyright (c) 2015 Janus Broch Mols. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
